b091ec17429027c49b9a7a6495373dc6b2293364
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2025-06-26 19:09:40 +0300

    deploy1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { OrdersProvider } from "./OrdersContext";
 import OrderPage from "./pages/OrderPage";
 import Dashboard from "./pages/Dashboard";
